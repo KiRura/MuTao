4a90c62ba2ea0f62dace1fbe0fee67eca3240629
KiRura <KiRura@noreply.com>
2024-04-07 06:32:36 +0900

    あ

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  name: "self", // Name of your application
+  name: "mutao", // Name of your application
   script: "main.js", // Entry point of your application
   interpreter: "~/.bun/bin/bun" // Path to the Bun interpreter
 };
