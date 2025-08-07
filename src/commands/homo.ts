77dc698c0ac7b2572d51d25ca12e75fa7e07c3d9
KiRura <57588049+KiRura@users.noreply.github.com>
2025-08-07 19:51:24 +0900

    homoライブラリをevex氏のフォークに, /homoの機能にテコ入れ

--- src/commands/homo.ts
+++ src/commands/homo.ts
@@ -1,8 +1,8 @@
+import { homo } from "@evex/h0mo";
 import {
 	ApplicationCommandOptionType,
 	ApplicationCommandType,
 } from "discord.js";
-import homo from "../lib/homo/homo";
 import type { Command } from ".";
 
 export default {
@@ -23,15 +23,30 @@ export default {
 	},
 	async execute(interaction) {
 		if (!interaction.isChatInputCommand()) return;
-		const num = interaction.options.getNumber("number") ?? 114514;
 
-		const res: string = homo(num);
+		const num = interaction.options.getNumber("number") ?? 114514;
+		const res = homo(num);
+
+		if (!res.success) {
+			await interaction.reply({
+				embeds: [
+					{
+						title: "(この数字を臭くできないなんて)やりませんねぇ...",
+						description: `${num}\n↓\n失敗`,
+					},
+				],
+			});
+
+			return;
+		}
+
+		const calcRes = Function(`return (${res.result});`)();
 
 		await interaction.reply({
 			embeds: [
 				{
 					title: "114514!!!",
-					description: `${num}\n↓\n\`${res}\`\n↓\n${Function(`return (${res});`)()}`,
+					description: `${num}\n↓\n\`${res.result}\`\n↓\n${calcRes}${num !== calcRes ? "\n-# (JavaScriptに正確性なんて)ないです" : ""}`,
 				},
 			],
 		});
